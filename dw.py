f29ad68a93b02714666f9040360c46c2f390c5fa
Martin Langa <167089903+elverolanga@users.noreply.github.com>
2025-03-04 19:42:08 +0200

    first commit

---
+++ dw.py
@@ -0,0 +1,14 @@
+pyimport os
+import requests
+
+urls = [
+]
+
+output_dir = './dwnl'
+
+for url in urls:
+    response = requests.get(url)
+    if response.status_code == 200:
+        file_path = os.path.join(output_dir, os.path.basename(url))
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
